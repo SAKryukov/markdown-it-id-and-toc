3bce2b8774e8d3f8359917c4c4a60ee07567cb07
SAKryukov <github@sakryukov.org>
2017-07-23 11:06:46 -0400

    Refactored plugin code to keep entry-point rule function reasonably small and put it on top

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -2,7 +2,7 @@
 
 const defaultOptions = {
     enableHeadingId: true,
-    includeLevel: [1, 2, 3, 4, 5, 6],
+    includeLevel: [2, 4, 5, 6],
     tocContainerClass: "toc",
     tocRegex: "^\\[\\]\\(toc\\)",
     excludeFromTocRegex: "\\[\\]\\(notoc\\)",
@@ -49,6 +49,92 @@ module.exports = function (md, userOptions) {
     // no magic function names:
     const tocFunctionNames = { open: "tocOpen", close: "tocClose", body: "tocBody" };
     const ruleName = "toc"; // works with null, but let's care about other plug-ins
+
+    // entry point:
+    md.core.ruler.before("inline", "buildToc", function (state) {
+        if (!options.enableHeadingId)   // inconsistent with having toc/no-toc tags, 
+            return;                     // so leave them as is
+        let tocRegexp = options.tocRegex;
+        if (tocRegexp.constructor != RegExp)
+            tocRegexp = new RegExp(options.tocRegex, "m");
+        // extra global check saves time if there is no match:
+        let doToc = false;
+        if (tocRegexp) {
+            const match = tocRegexp.exec(state.src);
+            doToc = match != null;
+        } //if
+        let excludeFromTocRegex = options.excludeFromTocRegex;
+        if (excludeFromTocRegex.constructor != RegExp)
+            excludeFromTocRegex = new RegExp(options.excludeFromTocRegex, "m");
+        //
+        const usedIds = { headings: {}, toc: {}, excludeFromToc: {} };
+        const idCounts = { headings: 0, toc: 0 };
+        const idSet = [];
+        buildIdSet(idSet, state.tokens, excludeFromTocRegex, usedIds);
+        addIdAttributes(idCounts, idSet);
+        if (!doToc) return;
+        createToc(state, usedIds, idCounts, idSet);
+        detectAndPushToc(tocRegexp);
+    }); //md.core.ruler.before
+
+    function buildIdSet(idSet, tokens, excludeFromTocRegex, usedIds) {
+        for (let index = 1; index < tokens.length; ++index) {
+            const token = tokens[index];
+            const headingTextToken = tokens[index - 1];
+            if (token.type !== "heading_close" || headingTextToken.type !== "inline") continue;
+            idSet.push(slugify(headingTextToken.content, usedIds));
+            if (excludeFromTocRegex) {
+                const oldContent = headingTextToken.content;
+                headingTextToken.content = headingTextToken.content.replace(excludeFromTocRegex, "");
+                if (oldContent !== headingTextToken.content)
+                    usedIds.excludeFromToc[index] = token;
+            } //if
+        } //loop
+    } //buildIdSet
+
+    function addIdAttributes(idCounts, idSet) {
+        const headingOpenPrevious = md.renderer.rules.heading_open;
+        md.renderer.rules.heading_open = function (tokens, index, userOptions, object, renderer) {
+            tokens[index].attrs = tokens[index].attrs || [];
+            let title = tokens[index + 1].children.reduce(function (accumulator, child) {
+                return accumulator + child.content;
+            }, "");
+            const headingSlug = idSet[idCounts.headings];
+            tokens[index].attrs.push(["id", headingSlug]);
+            ++idCounts.headings;
+            if (headingOpenPrevious)
+                return headingOpenPrevious.apply(this, arguments);
+            else
+                return renderer.renderToken.apply(renderer, arguments);
+            //SA!!! APPEND text to return to add prefix to heading content
+        }; //md.renderer.rules.heading_open
+    } //addIdAttributes
+
+    function createToc(state, usedIds, idCounts, idSet) {
+        md.renderer.rules[tocFunctionNames.open] = function (tokens, index) {
+            return util.format("<div class=\"%s\">", options.tocContainerClass);
+        }; // open
+        md.renderer.rules[tocFunctionNames.body] = function (tokens, index) {
+            return createTocTree(0, state.tokens, usedIds, idCounts, idSet)[1];
+        }; //body
+        md.renderer.rules[tocFunctionNames.close] = function (tokens, index) {
+            return "</div>";
+        }; //close
+    } //createToc
+
+    function detectAndPushToc(tocRegexp) {
+        md.inline.ruler.before("text", ruleName, function toc(state, silent) {
+            if (silent) return false;
+            const match = tocRegexp.exec(state.src);
+            if (!match) return false;
+            if (match.length < 1) return false;
+            state.push(tocFunctionNames.open, ruleName, 1);
+            state.push(tocFunctionNames.body, ruleName, 0);
+            state.push(tocFunctionNames.close, ruleName, -1);
+            state.src = "";
+            return true;
+        });
+    } //detectAndPushToc
 
     function createTocTree(pos, tokens, usedIds, idCounts, idSet) {
         let headings = [],
@@ -125,83 +211,5 @@ module.exports = function (md, userOptions) {
                         options.defaultListElementAttributeSet[index]);
         return util.format("<%s%s>%s</%s>", listTag, elementAttributes, headings.join(""), listTag);
     } //listElement
-
-    function buildIdSet(idSet, tokens, excludeFromTocRegex, usedIds) {
-        for (let index = 1; index < tokens.length; ++index) {
-            const token = tokens[index];
-            const headingTextToken = tokens[index - 1];
-            if (token.type !== "heading_close" || headingTextToken.type !== "inline") continue;
-            idSet.push(slugify(headingTextToken.content, usedIds));
-            if (excludeFromTocRegex) {
-                const oldContent = headingTextToken.content;
-                headingTextToken.content = headingTextToken.content.replace(excludeFromTocRegex, "");
-                if (oldContent !== headingTextToken.content)
-                    usedIds.excludeFromToc[index] = token;
-            } //if
-        } //loop
-    } //buildIdSet
-
-    md.core.ruler.before("inline", "buildToc", function (state) {
-        if (!options.enableHeadingId)   // inconsistent with having toc/no-toc tags, 
-            return;                     // so leave them as is
-        let tocRegexp = options.tocRegex;
-        if (tocRegexp.constructor != RegExp)
-            tocRegexp = new RegExp(options.tocRegex, "m");
-        // extra global check saves time if there is no match:
-        let doToc = false;
-        if (tocRegexp) {
-            const match = tocRegexp.exec(state.src);
-            doToc = match != null;
-        } //if
-        let excludeFromTocRegex = options.excludeFromTocRegex;
-        if (excludeFromTocRegex.constructor != RegExp)
-            excludeFromTocRegex = new RegExp(options.excludeFromTocRegex, "m");
-        //
-        const usedIds = { headings: {}, toc: {}, excludeFromToc: {} };
-        const idCounts = { headings: 0, toc: 0 };
-        const idSet = [];
-        buildIdSet(idSet, state.tokens, excludeFromTocRegex, usedIds);
-        // create TOC:
-        if (doToc) {
-            md.renderer.rules[tocFunctionNames.open] = function (tokens, index) {
-                return util.format("<div class=\"%s\">", options.tocContainerClass);
-            }; // open
-            md.renderer.rules[tocFunctionNames.body] = function (tokens, index) {
-                return createTocTree(0, state.tokens, usedIds, idCounts, idSet)[1];
-            }; //body
-            md.renderer.rules[tocFunctionNames.close] = function (tokens, index) {
-                return "</div>";
-            }; //close
-        } //if doToc
-        // add id attributes:
-        const headingOpenPrevious = md.renderer.rules.heading_open;
-        md.renderer.rules.heading_open = function (tokens, index, userOptions, object, renderer) {
-            tokens[index].attrs = tokens[index].attrs || [];
-            let title = tokens[index + 1].children.reduce(function (accumulator, child) {
-                return accumulator + child.content;
-            }, "");
-            const headingSlug = idSet[idCounts.headings];
-            tokens[index].attrs.push(["id", headingSlug]);
-            ++idCounts.headings;
-            if (headingOpenPrevious)
-                return headingOpenPrevious.apply(this, arguments);
-            else
-                return renderer.renderToken.apply(renderer, arguments);
-            //SA!!! APPEND text to return to add prefix to heading content
-        }; //md.renderer.rules.heading_open
-        // detect TOC location:
-        if (doToc)
-            md.inline.ruler.before("text", ruleName, function toc(state, silent) {
-                if (silent) return false;
-                const match = tocRegexp.exec(state.src);
-                if (!match) return false;
-                if (match.length < 1) return false;
-                state.push(tocFunctionNames.open, ruleName, 1);
-                state.push(tocFunctionNames.body, ruleName, 0);
-                state.push(tocFunctionNames.close, ruleName, -1);
-                state.src = "";
-                return true;
-            });
-    }); //md.core.ruler.before
 
 }; //module.exports
